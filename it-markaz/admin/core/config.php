d4827532340130ec205648bfb8e396baed649d09
davlatfarruxov <76444252+davlatfarruxov@users.noreply.github.com>
2023-05-02 18:02:09 +0500

    commit2

---
+++ it-markaz/admin/core/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+define('DB_NAME', "baza");
+define('DB_HOST', "localhost");
+define('DB_USER', "root");
+define('DB_PASS', "");
+define('URL', "http://it-markaz.uz");
+$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if (!$link) {
+    die("Connnection failed: ".mysqli_connect_error());
+}
+
+
+?>
---
+++ it-markaz/admin/core/model.php
@@ -0,0 +1,43 @@
+<?php require_once 'config.php'; ?>
+
+<?php 
+
+class Model 
+{
+    public $db;
+    public $table;
+    public $prod;
+    public function __construct()
+    {
+        require_once 'config.php';
+        $this->db=mysqli_connect("localhost", "root", "", "itmarkaz");
+    }
+
+    public function filtr($str)
+    {
+        return mysqli_real_escape_string($this->db, $str);
+    }
+    public function query($sql)
+    {
+        return mysqli_query($this->db, $sql);
+    }
+    public function select()
+    {
+        $sql = "SELECT * FROM ".$this->table." ORDER BY id DESC";
+        $back = mysqli_query($this->db, $sql);
+        return mysqli_fetch_all($back, MYSQLI_ASSOC);
+    }
+    public function selectOne ()
+    {
+        $sql = "SELECT * FROM ".$this->table." WHERE id=".$id;
+        $back = mysqli_query($this->db, $sql);
+        return mysqli_fetch_assoc($back);
+    }
+    public function delete($id)
+    {
+        $sql = "DELETE FROM ".$this ->table ."WHERE id=".$id;
+        return mysqli_query($this->db, $sql);
+    }
+
+}
+?>  
