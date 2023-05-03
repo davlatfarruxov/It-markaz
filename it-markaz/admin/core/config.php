ad112be47b610dfd15dad2cb60e68ec52f4e1213
Rufat <132185269+Rufatbek@users.noreply.github.com>
2023-05-03 16:45:37 +0500

    fgvb

--- it-markaz/admin/core/config.php
+++ it-markaz/admin/core/config.php
@@ -1,14 +1,13 @@
 <?php 
 
-define('DB_NAME', "baza");
-define('DB_HOST', "localhost");
-define('DB_USER', "root");
-define('DB_PASS', "");
-define('URL', "http://it-markaz.uz");
-$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if (!$link) {
-    die("Connnection failed: ".mysqli_connect_error());
-}
-
+    define('DB_NAME', "itmarkaz");
+    define('DB_HOST', "localhost");
+    define('DB_USER', "root");
+    define('DB_PASS', "");
+    define('URL', "http://it-markaz.uz");
+    $link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if (!$link) {
+        die("Connnection failed: ".mysqli_connect_error());
+    }
 
 ?>
---
+++ it-markaz/admin/core/newsController.php
@@ -0,0 +1,23 @@
+<?php 
+include_once 'model.php';
+class Yangilik extends Model
+{
+    public $table="yangiliklar";
+    public function insert($arr)
+    {
+        foreach ($arr as $key => $value){
+            $$key = $this->filtr($value);
+        }
+        if ($sarlavha == "" || $matn == "") {
+            return 409;
+        }
+
+        $sql = "INSERT INTO ".$this->table." (`sarlavha`, `matn`) VALUES ('$sarlavha','$matn')";
+
+        if ($this->query($sql)) {
+            return 200;
+        }else{
+            return 409;
+        }
+    }
+};
